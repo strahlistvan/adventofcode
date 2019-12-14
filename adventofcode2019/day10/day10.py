91eae5e9e4306316ac53a79d9e9d379d3283cc56
strahlistvan <strahlistvan0@gmail.com>
2019-12-14 21:17:13 +0100

    day 10 part 1

---
+++ adventofcode2019/day10/day10.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 22:43:53 2019
+
+@author: strahl
+"""
+
+import math
+
+with open('day10_input.txt') as fp:
+    input = fp.readlines()
+
+#Convert string list to list-in-list
+input_list = []
+for line in input:
+    input_list.append([ch for ch in line])
+print(input_list)
+
+asteroids = []
+for x in range(len(input_list)):
+    for y in range(len(input_list[x])):
+        if input_list[x][y] == '#':
+            asteroids.append({'x': x, 'y': y})
+
+max_detectable_count = 0
+for i in range(len(asteroids)):
+    candidate = asteroids[i]
+    asteroid_detect_list = []
+    for j in range(len(asteroids)):
+        other = asteroids[j]
+        if candidate == other:
+            continue
+
+        other['dy'] = candidate['y']-other['y']
+        other['dx'] = candidate['x']-other['x']
+        other['angle'] = round(math.atan2(other['dy'], other['dx']), 4)
+        other['dist'] = round(math.sqrt(other['dx']**2 + other['dy']**2), 4)
+        asteroid_detect_list.append(other)
+
+    detectable_count = len(set([x['angle'] for x in asteroid_detect_list]))
+    print(detectable_count)
+    if detectable_count > max_detectable_count:
+        max_detectable_count = detectable_count
+print('Maximal other asteroid detectable from a location: ' + str(max_detectable_count))
